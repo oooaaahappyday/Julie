51c14149e686d5c2df79f65d683d0db84c4688f3
Joseph Boisselier <joseph.boisselier@gmail.com>
2015-11-16 17:09:38 +0100

    commit de régulation

--- README.md
+++
@@ -1,4 +0,0 @@
-Julie
-=====
-
-A Symfony project created on October 9, 2015, 3:54 pm.
--- src/Julie/PlatformBundle/JuliePlatformBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Julie\PlatformBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class JuliePlatformBundle extends Bundle
-{
-}
