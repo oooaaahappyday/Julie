507deb3c8485c2b38abe2c156accfae18914446b
joseph boisselier <joseph.boisselier@gmail.com>
2016-06-08 16:46:44 +0200

    Ajout fichier manquants pour pouvoir merger

---
+++ web/js/galerieFlex.js
@@ -0,0 +1,59 @@
+
+$(document).ready(function() {
+    
+  $('#carousel').flexslider({
+    start: function(slider) {
+    if (slider.pagingCount == 1) slider.addClass('flex-centered');
+    },
+    before: function(slider) {
+    if (slider.pagingCount == 1) slider.addClass('flex-centered');
+    },
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    asNavFor: '#slider',
+    itemWidth: 135,
+    smoothHeight: true,
+    itemMargin: 4,
+    nextText: '',
+    prevText: '',
+    touch: true
+  });
+ 
+  $('#slider').flexslider({
+    before: function(slider, currentSlide) {
+        /*
+        document.getElementById("imageDetails").onclick = updateCurrentImage;
+        
+        function updateCurrentImage() {
+        var currentImage = slider.currentSlide;
+        console.log(currentImage);
+        return currentImage;
+        }
+        */
+    },
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel",
+    nextText: '',
+    prevText: '',
+    keyboard: true,
+    touch: true
+  });
+ 
+  $('#slider-admin').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel-vertical",
+    nextText: '',
+    prevText: '',
+    keyboard: true,
+    touch: true
+  });
+
+});
