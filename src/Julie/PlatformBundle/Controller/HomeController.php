13680da63c7c95a3b22887ff964cdd1f9f1460f8
joseph boisselier <joseph.boisselier@gmail.com>
2016-06-14 19:32:02 +0200

    new feature: contact form

--- src/Julie/PlatformBundle/Controller/HomeController.php
+++ src/Julie/PlatformBundle/Controller/HomeController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Julie\PlatformBundle\Entity\Galerie;
 use Julie\PlatformBundle\Entity\Categorie;
 use Julie\PlatformBundle\Entity\CategorieRepository;
+use Julie\PlatformBundle\Form\Type\ContactType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class HomeController extends Controller
@@ -24,29 +29,8 @@ class HomeController extends Controller
 			'id' => 1,
 	  	'listImage' => $listImage
 		));
-		/*
-		$content = $this
-		->get('templating')
-		->render('JuliePlatformBundle:Home:index.html.twig', array(
-			'name' 	=> 'Julie'
-			)
-		);
-		return new Response($content);
-		*/
 	}
-/*
-	public function indexAction()
-	{
-		
-		$content = $this
-		->get('templating')
-		->render('JuliePlatformBundle:Home:index.html.twig', array(
-			'name' => 'Julie'
-			)
-		);
-		return new Response($content);
-	}
-*/
+
 	public function aboutAction()
 	{
 		$content = $this
@@ -58,15 +42,38 @@ class HomeController extends Controller
 		return new Response($content);
 	}
 
-	public function contactAction()
+	/**
+	 *@Route("/contact", name="contact")
+	 *@Template
+	 */
+
+	public function contactAction(Request $request)
 	{
-		$content = $this
-		->get('templating')
-		->render('JuliePlatformBundle:Navbar:contact.html.twig', array(
-			'name' => 'Julie'
-			)
-		);
-		return new Response($content);
+		$form = $this->createForm(new ContactType());
+			if ($request->isMethod('POST')) {
+				$form->bind($request);
+					if ($form->isValid()) {
+						$message = \Swift_Message::newInstance()
+							->setSubject($form->get('sujet')->getData())
+							->setFrom($form->get('email')->getData())
+							->setTo('joseph.boisselier@gmail')
+							->setBody(
+									$this->renderView(
+										'JuliePlatformBundle:Mail:contact_mail.html.twig', array(
+												'ip' => $request->getClientIp(),
+												'name' => $form->get('name')->getData(),
+												'message' => $form->get('message')->getData()
+												)
+										)
+								);
+						$this->get('mailer')->send($message);
+						$request->getSession()->getFlashBag()->add('success', 'Votre mail a bien été envoyé, merci!');
+						return $this->redirect($this->generateUrl('home'));
+					}
+		}
+		return array(
+				'form' => $form->createView()
+			);
 	}
 
 	public function menuAction()
---
+++ src/Julie/PlatformBundle/Form/Type/ContactType.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Julie\PlatformBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Collection;
+
+class ContactType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $option)
+	{
+		$builder
+			->add('name', 'text', array(
+					'attr'	=> array(
+						'placeholder' => 'Quel est votre nom?',
+						'pattern'			=> '.{2,}' //minlength
+					)
+				))
+			->add('email', 'email', array(
+					'attr'	=> array(
+						'placeholder' => 'Que je puisse vous répondre.'
+					)
+				))
+			->add('sujet', 'text', array(
+					'attr'	=> array(
+						'placeholder'	=> 'Le sujet de votre message.',
+						'pattern'			=> '.{3,}'
+					)
+				))
+			->add('message', 'textarea', array(
+					'attr'	=> array(
+						'cols'				=> 100,
+						'rows'				=> 15,
+						'placeholder' => 'Votre message...'
+					)
+				))
+			->add('envoyer', 'submit')
+		;
+	}
+
+	public function setDefaultOptions (OptionsResolverInterface $resolver)
+	{
+		$collectionConstraint = new Collection(array(
+			'name' 		=> array(
+				new NotBlank(array('message' 	=> 'Votre nom est important.')),
+				new Length(array('min' 				=> 2 ))
+				),
+			'email' 	=> array(
+				new NotBlank(array('message' 	=> 'Votre email est important.')),
+				new Email(array('message' 		=> 'Adresse mail non valide.'))
+				),
+			'sujet' => array(
+				new NotBlank(array('message'	=> 'Le sujet est important.')),
+				new Length(array('min'				=> 3))
+				),
+			"message"	=> array(
+				new NotBlank(array('message'	=> 'Votre mail est vide.')),
+				new Length(array('min'				=> 5))
+				)
+			));
+			$resolver->setDefaults(array(
+				'constraints' => $collectionConstraint
+			));
+	}
+
+	public function getName()
+	{
+		return 'contact';
+	}
+}
